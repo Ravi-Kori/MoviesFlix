afb3fe1284ed4a77e0d786f0b0c06dbd01090cfc
Ravi-Kori <ravikori@gmail.com>
2025-11-29 15:04:53 +0530

    full code

---
+++ services/appwrite.ts
@@ -0,0 +1,68 @@
+import { Client, TablesDB, Query, ID } from "react-native-appwrite";
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const TABLE_ID = process.env.EXPO_PUBLIC_APPWRITE_TABLE_ID!;
+
+const client = new Client()
+  .setEndpoint('https://cloud.appwrite.io/v1')
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+
+const database = new TablesDB(client);
+
+export const UpdateSearchCount = async (query: string, movie: Movie) => {
+  try {
+    // List rows matching the search term
+    const result = await database.listRows({
+      databaseId: DATABASE_ID,
+      tableId: TABLE_ID,
+      queries: [Query.equal("searchTerm", query)],
+    });
+    
+    if (result.rows.length > 0) {
+        // Row exists, update count
+        const existingMovie = result.rows[0];
+        
+        await database.updateRow({
+            databaseId: DATABASE_ID,
+            tableId: TABLE_ID,
+            rowId: existingMovie.$id,
+            data: {
+                count: (existingMovie.count || 0) + 1,
+            },
+        });
+    } else {
+        // Row does not exist, create new
+        await database.createRow({
+            databaseId: DATABASE_ID,
+            tableId: TABLE_ID,
+            rowId: ID.unique(),
+            data: {
+                searchTerm: query,
+                movie_id: movie.id,
+                title: movie.title,
+                count: 1,
+                poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+            },
+        });
+    }
+} catch (error) {
+    console.error("Error updating search count:", error);
+    throw error;
+}
+};
+
+export const getTrendingMovies = async () : Promise<TrendingMovie[] | undefined> => {
+    try{
+        const result = await database.listRows({
+          databaseId: DATABASE_ID,
+          tableId: TABLE_ID,
+          queries: [Query.limit(5),Query.orderDesc('count')],
+        });
+
+        return result.rows as unknown as TrendingMovie[]
+
+    } catch (error) {
+        console.log(error)
+        return undefined
+    }
+}
